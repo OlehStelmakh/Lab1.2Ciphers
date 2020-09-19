8f275febb6c1aac58197a3b9c497cdd31d0e56b9
Oleh Stelmakh <olehstelmakh@gmail.com>
2020-09-19 13:57:58 +0300

    Added tests

--- Lab1.2Test/ModuleNCipherTest.cs
+++ Lab1.2Test/ModuleNCipherTest.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using Lab1._2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace Lab1._2Test
 {
+    [TestClass]
     public class ModuleNCipherTest
     {
-        public ModuleNCipherTest()
+        [TestMethod]
+        public void ModuleNCipherShouldWorkCorrectlyForHugePhrases()
         {
+            //first
+            string text = "The code above uses the much criticised System.Random method to " +
+                "select swap candidates. It's fast but not as random as it should be.";
+            string key = "password";
+            var moduleNCipher = new ModuleNCipher(AlphabetManager.FullMixedAlphabetList);
+            var encryptedText = moduleNCipher.Encrypt(text, key);
+            var decryptedText = moduleNCipher.Decrypt(encryptedText, key);
+
+            Assert.AreEqual(encryptedText, "зIXSZГVIPBUЖПTRYЗFЇSНWWDВVVАWRИMИJVБМTVDжZЇЙББJuQOW" +
+                "ЖИOДIИIЖWWЗЄDЗFДXZЗRWЙBЗSZPЕHYETЙБЖJDxUmЇWUSWИAUКНOЕSИATЇWЄSRTPЕSXЖRMИAЇАЇИГHPCXK");
+            Assert.AreEqual(decryptedText, text);
+
+            //second
+            text = "Donald John Trump (born June 14, 1946) is the 45th and current president " +
+                "of the United States. Before entering politics, he was a businessman and television personality.";
+            key = "Hillary Diane Rodham Clinton";
+
+            encryptedText = moduleNCipher.Encrypt(text, key);
+            decryptedText = moduleNCipher.Decrypt(encryptedText, key);
+
+            Assert.AreEqual(encryptedText, "KXZMMVYirQONxRїБTH)OOtZIwЛВSg- cAEP3?gAWX]іWIH5BTjLJБXOQ" +
+                "бАГQOЇYPuNTWIEдЗDWGMTjQIжЖXЖlMLгUSПEvAAoJFеЄIHFАTgГRБАOГvUUЕJUО9cQFNБAиOEHCЖSkZNЄЙБOuIMZERПEoNWWXIеВDXFДSqZJZВЗЙ[");
+            Assert.AreEqual(decryptedText, text);
+
+            //third
+            text = "Виходячи із смертельної небезпеки, яка нависла була над " +
+                "Україною в зв'язку з державним переворотом в СРСР 19 серпня 1991 року...";
+            key = "24 серпня 1991 рік";
+
+            encryptedText = moduleNCipher.Encrypt(text, key);
+            decryptedText = moduleNCipher.Decrypt(encryptedText, key);
+
+            Assert.AreEqual(encryptedText, "PxХS8ЕZGЯІV8ЇВЕTM]боНS=РPCNЕVИWАИіІZаo]R0CIRАА0SКБАРH'r3тO" +
+                "FAKNГЮ0T8VВкWCаі]JЕEEQTАPЖАВ]S\\NsqОUDVQMЯВ0іиєпРXД1зЕUEQДНП8ФФ8ЄОNNЕхц");
+            Assert.AreEqual(decryptedText, text);
         }
     }
 }
--- Lab1.2Test/SingleBlockCipherTest.cs
+++ Lab1.2Test/SingleBlockCipherTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Lab1._2;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Lab1._2Test
@@ -6,8 +8,45 @@ namespace Lab1._2Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SingleBlockCipherShouldWorkCorreclyForHugePhrases()
         {
+            string text = "Also feel free to tell me why doing it this way is a horrible idea.";
+            var singleBlockCipher = new SingleBlockCipher();
+            var key = new List<int>() { 1, 3, 0, 2, 5, 9, 4, 8, 6, 7 };
+            var encryptedText = singleBlockCipher.Encrypt(text.ToLower(), key);
+            var decryptedText = singleBlockCipher.Decrypt(encryptedText, key);
+            Assert.AreEqual(encryptedText, "loasf  leerefete to lml   eywhondi tg iti hsaswiy ah  rlobri deia e . ");
+            Assert.AreEqual(decryptedText.Substring(0, text.Length), text.ToLower());
+
+            text = "Инициализирует новый экземпляр класса String значением, " +
+                "определенным указателем на массив 8-разрядных целых чисел со знаком.";
+            key = new List<int>() { 6, 4, 0, 2, 3, 1, 5 };
+            encryptedText = singleBlockCipher.Encrypt(text.ToLower(), key);
+            decryptedText = singleBlockCipher.Decrypt(encryptedText, key);
+            Assert.AreEqual(encryptedText, "лииицнатуиирзе ы овнйлмэзекпсля краitс sаrчнn зgа меиен," +
+                "лдорепеуменын лткзааемае нм 8васис др-азрялцнх ыееиы чхсн лсо з .аомк ");
+            Assert.AreEqual(decryptedText.Substring(0, text.Length), text.ToLower());
+
+            text = "The code above uses the much criticised System.Random method to " +
+                "select swap candidates. It's fast but not as random as it should be.";
+            key = new List<int>() { 7, 10, 2, 3, 5, 8, 0, 1, 9, 4, 6 };
+            encryptedText = singleBlockCipher.Encrypt(text.ToLower(), key);
+            decryptedText = singleBlockCipher.Decrypt(encryptedText, key);
+            Assert.AreEqual(encryptedText, "ebe o thacdshe s ovtuectmuhre ic stisdyicse om.rnmem " +
+                "adoeho  etsdtacctsple  we diasan.dtsb'sftit  aar ntsut o stdo  anima .holb seud");
+            Assert.AreEqual(decryptedText.Substring(0, text.Length), text.ToLower());
+        }
+
+        [TestMethod]
+        public void SingleBlockCipherShouldWorkCorreclyForSmallPhrases()
+        {
+            string text = "Пароль";
+            var singleBlockCipher = new SingleBlockCipher();
+            var key = new List<int>() { 1, 3, 2, 0};
+            var encryptedText = singleBlockCipher.Encrypt(text.ToLower(), key);
+            Assert.AreEqual(encryptedText, "аорпь  л");
+            var decryptedText = singleBlockCipher.Decrypt(encryptedText, key);
+            Assert.AreEqual(decryptedText.Substring(0, text.Length), text.ToLower());
         }
     }
 }
